963bb757e821d27b2aa6b9fde92ac333ae32bfcd
AsianUser <90284651+AsianUser@users.noreply.github.com>
2023-01-28 00:43:20 -0800

    create new files and folders
    copied Alex's code
    prepared for command based prog

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,84 +4,100 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
-import com.revrobotics.*;
-import frc.robot.Drive.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the manifest
- * file in the resource
- * directory.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  private CANSparkMax one; // counterclockwise
-  private CANSparkMax two; // counterclockwise
-  private CANSparkMax three; // clockwise
-  private CANSparkMax four; // counterclockwise
-  private CANSparkMax five; // counterclockwise
-  private CANSparkMax six; // clockwise
-  private DriveSide right;
-  private DriveSide left;
-  private PS4Controller con;
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
 
   /**
-   * This function is run when the robot is first started up and should be used
-   * for any
+   * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    this.one = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-    this.two = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-    this.three = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-    this.four = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
-    this.five = new CANSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
-    this.six = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    right = new DriveSide(one, two, three); 
-    left = new DriveSide(four, five, six);
-    con = new PS4Controller(0);
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
-  /** This function is run once each time the robot enters autonomous mode. */
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-  }
+  public void autonomousPeriodic() {}
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
   @Override
   public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
   }
 
-  /** This function is called periodically during teleoperated mode. */
+  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    // three.setVoltage(2);
-    right.set_power(con.getRightY());
-    left.set_power(con.getLeftY());
-  }
+  public void teleopPeriodic() {}
 
-  /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
   }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {
-  }
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
